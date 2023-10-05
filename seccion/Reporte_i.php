b19e4a8b3f4b171190af1e44ab8219aedc12c521
MonicaMDiaz <124890620+MonicaMDiaz@users.noreply.github.com>
2023-10-05 13:04:24 -0500

    5/10am

--- seccion/Reporte_i.php
+++ seccion/Reporte_i.php
@@ -37,20 +37,6 @@ h1 {
 }
 </style>
 <?php
-function verificarCampo($campo, $ficha) {
-    if ($ficha[$campo]=='Mal'){ ?>
-<th><?php echo $campo; ?>:</th>
-<td>Arreglar</td>
-<?php }
-    elseif($ficha[$campo]=='Regular'){?>
-<th><?php echo $campo; ?>:</th>
-<td>Verificar</td>
-<?php } 
-    elseif($ficha[$campo]=='N/A'){?>
-<th><?php echo $campo; ?>:</th>
-<td>Poner</td>
-<?php } 
-  }
   
 include_once '../databases/BD.php';
 $conexionBD=BD::crearInstancia();
@@ -110,34 +96,8 @@ if($accion!=''){
 <br>
 <h2></h2>
 <h1>Plan de acción</h1>
-<table class='table1' style="width:100%" border="3">
-    <tr>
-        <th rowspan="2" class='table1'>Actividad</th>
-        <th colspan="2">Tiempo</th>
-        <th rowspan="2" class='table1'>Responsable</th>
-        <th rowspan="2" class='table1'>Resultados esperados</th>
-    </tr>
-    <tr>
-        <td class='table1'>Inicio</td>
-        <td class='table1'>Final</td>
-    </tr>
-    <tr>
-        <td class='table1' contenteditable="true"> <?php echo nl2br($ficha["observacion"])?> </td>
-        <td class='table1'><input type="date" name="inicio"></td>
-        <td class='table1'><input type="date"></td>
-        <td class='table1'><span contenteditable="true"> Mónica Diaz</span></td>
-        <td class='table1' contenteditable="true"> Se espera que xxxx xxxx xxx </td>
-    </tr>
-</table>
-<div class='buttons'>
-    <form action="" method="post">
-        <div class="btn-group" role="group" aria-label="" style="float: right">
-            <button type="submit" name="accion" value="Volver" class="btn btn-secundary">Volver</button>
-            <button type="submit" name="accion" value="Imprimir" class="btn btn-secundary">Imprimir</button>
-        </div>
-    </form>
-</div>
-<form method="post" action="imprimir.php">
+
+<form method="post" action="imprimir.php?id=<?php echo $id; ?>">
 
     <table class='table1' style="width:100%" border="3">
         <tr>
@@ -163,14 +123,11 @@ if($accion!=''){
         </tr>
     </table>
     <div class='buttons'>
-        <form action="imprimir.php" method="post" style="display: inline;">
-            <input type="hidden" name="id" value="<?php echo $id; ?>">
-            <button type="submit" name="accion" value="Imprimir" class="btn btn-secondary">Imprimir</button>
-        </form>
-        <form action="ficha_i.php" method="get" style="display: inline;">
-            <button type="submit" name="accion" value="Volver" class="btn btn-secondary">Volver</button>
+        <form action="" method="post">
+            <div class="btn-group" role="group" aria-label="" style="float: right">
+                <button type="submit" name="accion" value="Imprimir" class="btn btn-secundary">Imprimir</button>
+            </div>
         </form>
     </div>
-
 
     <?php include("../templates/pie.php"); ?>
--- seccion/imprimir.php
+++ seccion/imprimir.php
@@ -1,55 +1,44 @@
 <?php
 require('../librerias/fpdf.php');
-include_once '../databases/BD.php';
+require('../librerias/font/times.php');
+/*include_once '../databases/BD.php';
 $conexionBD=BD::crearInstancia();
-//print_r($_GET);
+
 if (isset($_POST['inicio'])) {
     $inicio = $_POST['inicio'];}
-    echo $inicio;
-$id = isset($_POST['id']) ? $_POST['id'] : null;
+
 $id = $_GET['id'];
 $sql = "SELECT * FROM datos INNER JOIN inventario ON datos.id = inventario.id WHERE datos.id = :id";
 //"SELECT datos.placa, datos.id,datos.Empresa,datos.Nombre, datos.fecha, inventario.Estado FROM datos INNER JOIN inventario ON datos.id = inventario.id WHERE inventario.Estado LIKE '%".$buscar_valor."%'";   
 $consulta = $conexionBD->prepare($sql);
 $consulta->bindParam(':id', $id);
 $consulta->execute();
-$ficha = $consulta->fetch(PDO::FETCH_ASSOC);
-print_r($ficha);
+$ficha = $consulta->fetch(PDO::FETCH_ASSOC);*/
+//print_r($ficha);
 
-/*$pdf = new FPDF();
+$pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
-// Agrega un título
-$pdf->Cell(0, 10, 'Informe de Datos', 0, 1, 'C');
+$pdf->Cell(40,10,utf8_decode('¡Hóáéla, Mundo!'));
+$pdf->Output();
 
-// Imprime la información de la tabla
-$pdf->Cell(0, 10, 'ID de bus: ' . $ficha['id'], 0, 1);
-$pdf->Cell(0, 10, 'Empresa: ' . $ficha['Empresa'], 0, 1);
-$pdf->Cell(0, 10, 'Placa: ' . $ficha["placa"], 0, 1);
-$pdf->Output('Informe.pdf', 'I');
-//$pdf->Output();
-
-
-$id = $_GET['id'];
-
-// Consulta la información de la ficha
-$sql = "SELECT * FROM datos INNER JOIN inventario ON datos.id = inventario.id WHERE datos.id = :id";
-$conexionBD = BD::crearInstancia();
-$consulta = $conexionBD->prepare($sql);
-$consulta->bindParam(':id', $id);
-$consulta->execute();
-$ficha = $consulta->fetch(PDO::FETCH_ASSOC);
-
-$fechaInicio = $_POST['fechaInicio'];
-$fechaFin = $_POST['fechaFin'];
 // Crea un nuevo objeto FPDF
-$pdf = new FPDF();
+/*$pdf = new FPDF();
 
 // Agrega una nueva página
 $pdf->AddPage();
 
-// Establece la fuente
-$pdf->SetFont('Arial', '', 12);
+// Agrega la fuente Arial
+// Comprueba si el navegador admite UTF-8
+if (mb_detect_encoding($_SERVER['HTTP_ACCEPT_LANGUAGE'], 'UTF-8', true)) {
+    // Establece el juego de caracteres como UTF-8
+    mb_internal_encoding('UTF-8');
+    // Establece la configuración de salida como UTF-8
+    mb_http_output('UTF-8');
+}
+
+// Establece la fuente Arial
+$pdf->SetFont('Arial','',12);
 
 // Imprime el título de la ficha
 $pdf->Cell(0, 10, 'Ficha de inspección de bus', 0, 1, 'C');
@@ -65,24 +54,28 @@ $pdf->Cell(0, 10, 'Fecha y hora: ' . $ficha['fecha'], 0, 1, 'L');
 // Imprime el plan de acción
 $pdf->Cell(0, 10, 'Plan de acción', 0, 1, 'C');
 
+// Obtiene el ancho del texto de la actividad
+$pdf->SetWidths(array(40, 20, 20, 40, 40));
+
 $pdf->Cell(40, 10, 'Actividad', 1, 0, 'C');
-$pdf->Cell(20, 10, $fechaInicio, 1, 0, 'L');
-$pdf->Cell(20, 10, $fechaFin, 1, 0, 'L');
+$pdf->Cell(20, 10, 'inicio', 1, 0, 'L');
+$pdf->Cell(20, 10, 'Fin', 1, 0, 'L');
 $pdf->Cell(40, 10, 'Responsable', 1, 0, 'C');
 $pdf->Cell(40, 10, 'Resultados esperados', 1, 1, 'C');
 
-$pdf->Cell(40, 10, $ficha['observacion'], 1, 0, 'L');
+// Imprime el contenido de la tabla
+$pdf->MultiCell(0, 5, $ficha['observacion'], 1, 'L', false, false);
 $pdf->Cell(20, 10, '', 1, 0, 'L');
 $pdf->Cell(20, 10, '', 1, 0, 'L');
 $pdf->Cell(40, 10, 'Mónica Díaz', 1, 0, 'L');
 $pdf->Cell(40, 10, '', 1, 1, 'L');
 
 // Genera el PDF
-$pdf->Output('ficha.pdf', 'F');
+$pdf->Output('Reporte.pdf', 'F');
 
 header('Content-type: application/pdf');
 header('Content-Disposition: inline; filename="ficha.pdf"');
 
-readfile('ficha.pdf');*/
+readfile('Reporte.pdf');*/
 
 ?>
