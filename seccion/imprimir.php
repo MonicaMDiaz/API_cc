fa9c17a9808e003b34e982376afa3bd0c43d6aa1
MonicaMDiaz <124890620+MonicaMDiaz@users.noreply.github.com>
2023-10-11 16:08:34 -0500

    11/10

--- seccion/imprimir.php
+++ seccion/imprimir.php
@@ -1,7 +1,8 @@
 <?php
 require('../librerias/fpdf.php');
 require('../librerias/font/times.php');
-/*include_once '../databases/BD.php';
+//require('../librerias/mc_table.php');
+include_once '../databases/BD.php';
 $conexionBD=BD::crearInstancia();
 
 if (isset($_POST['inicio'])) {
@@ -13,69 +14,52 @@ $sql = "SELECT * FROM datos INNER JOIN inventario ON datos.id = inventario.id WH
 $consulta = $conexionBD->prepare($sql);
 $consulta->bindParam(':id', $id);
 $consulta->execute();
-$ficha = $consulta->fetch(PDO::FETCH_ASSOC);*/
+$ficha = $consulta->fetch(PDO::FETCH_ASSOC);
 //print_r($ficha);
 
-$pdf = new FPDF();
-$pdf->AddPage();
-$pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10,utf8_decode('¡Hóáéla, Mundo!'));
-$pdf->Output();
-
 // Crea un nuevo objeto FPDF
-/*$pdf = new FPDF();
+$pdf = new FPDF();
 
 // Agrega una nueva página
 $pdf->AddPage();
-
-// Agrega la fuente Arial
-// Comprueba si el navegador admite UTF-8
-if (mb_detect_encoding($_SERVER['HTTP_ACCEPT_LANGUAGE'], 'UTF-8', true)) {
-    // Establece el juego de caracteres como UTF-8
-    mb_internal_encoding('UTF-8');
-    // Establece la configuración de salida como UTF-8
-    mb_http_output('UTF-8');
-}
-
 // Establece la fuente Arial
 $pdf->SetFont('Arial','',12);
-
+//$pdf->Cell(40,10,utf8_decode('¡Hóáéla, Mundo!'));
 // Imprime el título de la ficha
-$pdf->Cell(0, 10, 'Ficha de inspección de bus', 0, 1, 'C');
+$pdf->Cell(0,10,utf8_decode('Plan de acción'), 0, 1, 'C');
 
 // Imprime la información de la ficha
 $pdf->Cell(0, 10, 'ID de bus: ' . $ficha['id'], 0, 1, 'L');
 $pdf->Cell(0, 10, 'Empresa: ' . $ficha['Empresa'], 0, 1, 'L');
 $pdf->Cell(0, 10, 'Placa: ' . $ficha['placa'], 0, 1, 'L');
-$pdf->Cell(0, 10, 'Nombre conductor: ' . $ficha['Nombre'], 0, 1, 'L');
-$pdf->Cell(0, 10, 'Identificación: ' . $ficha['nit'] . ' ' . $ficha['nid'], 0, 1, 'L');
+$pdf->Cell(0, 10, utf8_decode('Nombre conductor: ' . $ficha['Nombre']), 0, 1, 'L');
+$pdf->Cell(0, 10, utf8_decode('Identificación: ' . $ficha['nit'] . ' ' . $ficha['nid']), 0, 1, 'L');
 $pdf->Cell(0, 10, 'Fecha y hora: ' . $ficha['fecha'], 0, 1, 'L');
 
-// Imprime el plan de acción
-$pdf->Cell(0, 10, 'Plan de acción', 0, 1, 'C');
+$pdf->Cell(0, 10, '', 0, 1, 'L');
 
-// Obtiene el ancho del texto de la actividad
-$pdf->SetWidths(array(40, 20, 20, 40, 40));
-
-$pdf->Cell(40, 10, 'Actividad', 1, 0, 'C');
-$pdf->Cell(20, 10, 'inicio', 1, 0, 'L');
-$pdf->Cell(20, 10, 'Fin', 1, 0, 'L');
+$pdf->Cell(45, 10, 'Actividad', 1, 0, 'C'); 
+$pdf->Cell(25, 10, 'inicio', 1, 0, 'C');
+$pdf->Cell(25, 10, 'Fin', 1, 0, 'C');
 $pdf->Cell(40, 10, 'Responsable', 1, 0, 'C');
-$pdf->Cell(40, 10, 'Resultados esperados', 1, 1, 'C');
+$pdf->Cell(50, 10, 'Resultados esperados', 1, 1, 'C');
+// Imprime el contenido de la tabla $pdf->$pdf->MultiCell(45, 10, utf8_decode($ficha['observacion']), 1,'L',0);
+$pdf->MultiCell(45, 10, utf8_decode($ficha['observacion']), 1, 'L', 0);
+$pdf->MultiCell(25, 10, $inicio, 1, 'L', 0);
+$pdf->MultiCell(25, 10, '', 1, 'L', 0);
+$pdf->MultiCell(40, 10, utf8_decode('Mónica Díaz'), 1, 'C', 0);
+$pdf->MultiCell(50, 10, utf8_decode(''), 1, 'L', 0);
 
-// Imprime el contenido de la tabla
-$pdf->MultiCell(0, 5, $ficha['observacion'], 1, 'L', false, false);
-$pdf->Cell(20, 10, '', 1, 0, 'L');
-$pdf->Cell(20, 10, '', 1, 0, 'L');
-$pdf->Cell(40, 10, 'Mónica Díaz', 1, 0, 'L');
-$pdf->Cell(40, 10, '', 1, 1, 'L');
+$pdf->SetWidths(array(30,40,50,60)); // Definir los anchos de cada columna
 
-// Genera el PDF
+// Llenar la tabla con algunos datos
+$pdf->Row(array(utf8_decode($ficha['observacion']),"Otro texto largo","Más texto","Y más texto"));
+
 $pdf->Output('Reporte.pdf', 'F');
 
 header('Content-type: application/pdf');
-header('Content-Disposition: inline; filename="ficha.pdf"');
+header('Content-Disposition: inline; filename="Reporte.pdf"');
 
-readfile('Reporte.pdf');*/
+readfile('Reporte.pdf');
 
 ?>
